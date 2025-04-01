78d94c420562b0bd512692b6ef862cd16f324274
isabel zimmerman <isabel.zimmerman@posit.co>
2025-04-01 00:47:38 +0000

    update files

--- pixar_movies.py
+++ pixar_movies.py
@@ -1,3 +1,10 @@
+# /// script
+# dependencies = [
+#   "pandas",
+#   "matplotlib",
+# ]
+# ///
+
 import pandas as pd
 
 url = 'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-03-11/pixar_films.csv'
